cad5d7b0ca15c17be8c6cba1c920e3c189bc16bc
Richard Nyström <ricny046@gmail.com>
2012-09-16 21:48:58 +0200

    Refactored the specs for speed and clarity

--- spec/rika_spec.rb
+++ spec/rika_spec.rb
@@ -4,26 +4,29 @@ require 'spec_helper'
 
 describe Rika::Parser do
   # TODO: add specs for file not found, unknown mimetypes, filestreams and URLs
+  
+  before(:all) do
+    @txt_parser = Rika::Parser.new(file_path("text_file.txt"))
+    @docx_parser = Rika::Parser.new(file_path("document.docx"))
+    @pdf_parser = Rika::Parser.new(file_path("document.pdf"))
+    @image_parser = Rika::Parser.new(file_path("image.jpg"))
+  end
 
   describe '#content' do
     it "should return the content in a text file" do
-      parser = Rika::Parser.new(file_path("text_file.txt"))
-      parser.content.should == "First they ignore you, then they ridicule you, then they fight you, then you win.\n"
+      @txt_parser.content.should == "First they ignore you, then they ridicule you, then they fight you, then you win.\n"
     end
 
     it "should return the content in a docx file" do
-      parser = Rika::Parser.new(file_path("document.docx"))
-      parser.content.should == "First they ignore you, then they ridicule you, then they fight you, then you win.\n"
+      @docx_parser.content.should == "First they ignore you, then they ridicule you, then they fight you, then you win.\n"
     end
 
-    it "should return the content in a pdf file" do
-      parser = Rika::Parser.new(file_path("document.pdf"))
-      parser.content.strip.should == "First they ignore you, then they ridicule you, then they fight you, then you win."
+    it "should return the content in a pdf file" do 
+      @pdf_parser.content.strip.should == "First they ignore you, then they ridicule you, then they fight you, then you win."
     end
 
     it "should return no content for an image" do
-      parser = Rika::Parser.new(file_path("image.jpg"))
-      parser.content.should be_empty
+      @image_parser.content.should be_empty
     end
   end
 
@@ -32,48 +35,40 @@ describe Rika::Parser do
   # have tests for all file formats it supports so we won't retest that
   describe '#metadata' do
     it "should return nil if metadata field does not exists" do
-      parser = Rika::Parser.new(file_path("text_file.txt"))
-      parser.metadata["nonsense"].should be_nil
+      @txt_parser.metadata["nonsense"].should be_nil
     end
 
     it "should return metadata from a text file" do
-      parser = Rika::Parser.new(file_path("text_file.txt"))
-      parser.metadata["filename"].should == "text_file.txt"
+      @txt_parser.metadata["filename"].should == "text_file.txt"
     end
 
     it "should return metadata from a docx file" do
-      parser = Rika::Parser.new(file_path("document.docx"))
-      parser.metadata["Page-Count"].should == "1"
+      @docx_parser.metadata["Page-Count"].should == "1"
     end
 
     it "should return metadata from a pdf file" do
-      parser = Rika::Parser.new(file_path("document.pdf"))
-      parser.metadata["title"].should == "A simple title"
+      @pdf_parser.metadata["title"].should == "A simple title"
     end
 
     it "should return metadata from an image" do
-      parser = Rika::Parser.new(file_path("image.jpg"))
-      parser.metadata["Image Height"].should == "72 pixels"
-      parser.metadata["Image Width"].should == "72 pixels"
+      @image_parser.metadata["Image Height"].should == "72 pixels"
+      @image_parser.metadata["Image Width"].should == "72 pixels"
     end
   end
 
   describe '#available_metadata' do
     it "should return available metadata fields" do
-      parser = Rika::Parser.new(file_path("text_file.txt"))
-      parser.available_metadata.should_not be_empty
+      @txt_parser.available_metadata.should_not be_empty
     end
   end
 
   describe '#metadata_exists?' do
     it "should return false if metadata does not exists" do
-      parser = Rika::Parser.new(file_path("text_file.txt"))
-      parser.metadata_exists?("title").should == false
+      @txt_parser.metadata_exists?("title").should == false
     end
 
     it "should return true if metadata exists" do
-      parser = Rika::Parser.new(file_path("document.docx"))
-      parser.metadata_exists?("title").should == true
+      @docx_parser.metadata_exists?("title").should == true
     end
   end
 end
