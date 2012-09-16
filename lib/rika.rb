a29717221daef8b1689ba57b483c0da42373586a
Richard Nyström <ricny046@gmail.com>
2012-09-16 18:48:51 +0200

    First commit using actual Java integration.

--- lib/rika.rb
+++ lib/rika.rb
@@ -1,12 +1,38 @@
+raise "You need to run JRuby to use Rika" unless RUBY_PLATFORM =~ /java/
+
 require "rika/version"
+require 'java' 
+
+Dir[File.join(File.dirname(__FILE__), "*.jar")].each do |jar|
+  require jar
+end
 
 module Rika
+	import org.apache.tika.sax.BodyContentHandler
+	import org.apache.tika.parser.AutoDetectParser
+	import org.apache.tika.metadata.Metadata
+
   class Parser
+  	
   	def initialize(filename)
+  		@filename = filename
+			self.perform
   	end
 
   	def content
-  		"First they ignore you, then they ridicule you, then they fight you, then you win."
+  		@content.to_s
   	end
+
+  	protected
+		
+		def perform
+			input_stream = java.io.FileInputStream.new(java.io.File.new(@filename))
+			@metadata = Metadata.new
+			@metadata.set("filename", File.basename(@filename))
+			@parser = AutoDetectParser.new
+			@content = BodyContentHandler.new
+			@parser.parse(input_stream, @content, @metadata)
+			input_stream.close
+		end
   end
 end
--- spec/rika_spec.rb
+++ spec/rika_spec.rb
@@ -6,8 +6,9 @@ describe Rika::Parser do
 
   describe '#content' do
     it "should return the content in a file" do
-      parser = Rika::Parser.new(@filename)
-      parser.content.should == "First they ignore you, then they ridicule you, then they fight you, then you win."
+
+      parser = Rika::Parser.new(file_path("text_file.txt"))
+      parser.content.should == "First they ignore you, then they ridicule you, then they fight you, then you win.\n"
     end
   end
 end
