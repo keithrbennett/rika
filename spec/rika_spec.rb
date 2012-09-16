fda0c2dd500ae2a92b0bc913a2c00756b161dd83
Richard Nyström <ricny046@gmail.com>
2012-09-16 19:28:25 +0200

    Added specs to test common file formats.

--- spec/rika_spec.rb
+++ spec/rika_spec.rb
@@ -3,12 +3,27 @@
 require 'spec_helper'
 
 describe Rika::Parser do
+  # TODO: add specs for file not found, unknown mimetypes, filestreams and URLs
 
   describe '#content' do
-    it "should return the content in a file" do
-
+    it "should return the content in a text file" do
       parser = Rika::Parser.new(file_path("text_file.txt"))
       parser.content.should == "First they ignore you, then they ridicule you, then they fight you, then you win.\n"
+    end
+
+    it "should return the content in a doc file" do
+      parser = Rika::Parser.new(file_path("document.doc"))
+      parser.content.should == "First they ignore you, then they ridicule you, then they fight you, then you win.\n"
+    end
+
+    it "should return the content in a pdf file" do
+      parser = Rika::Parser.new(file_path("document.doc"))
+      parser.content.should == "First they ignore you, then they ridicule you, then they fight you, then you win.\n"
+    end
+
+    it "should return no content for an image" do
+      parser = Rika::Parser.new(file_path("image.jpg"))
+      parser.content.should be_empty
     end
   end
 end
