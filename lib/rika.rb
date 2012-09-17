81c0a63e613fa46ee70603c73628b9473b2e1413
Richard Nyström <ricny046@gmail.com>
2012-09-17 18:16:34 +0200

    Converting identation to spaces

--- lib/rika.rb
+++ lib/rika.rb
@@ -8,61 +8,61 @@ Dir[File.join(File.dirname(__FILE__), "../target/dependency/*.jar")].each do |ja
 end
 
 module Rika
-	import org.apache.tika.sax.BodyContentHandler
-	import org.apache.tika.parser.AutoDetectParser
-	import org.apache.tika.metadata.Metadata
+  import org.apache.tika.sax.BodyContentHandler
+  import org.apache.tika.parser.AutoDetectParser
+  import org.apache.tika.metadata.Metadata
 
   class Parser
-  	
-  	def initialize(filename)
-  		if File.exists?(filename)
-  			@filename = filename
-				self.perform
-			else
-				raise IOError, "File does not exist"
-			end
-  	end
+    
+    def initialize(filename)
+      if File.exists?(filename)
+        @filename = filename
+        self.perform
+      else
+        raise IOError, "File does not exist"
+      end
+    end
 
-  	def content
-  		@content.to_s
-  	end
+    def content
+      @content.to_s
+    end
 
-  	def metadata
-  		metadata_hash = {}
-  		
-  		@metadata.names.each do |name|
-  			metadata_hash[name] = @metadata.get(name) 
-  		end
+    def metadata
+      metadata_hash = {}
+      
+      @metadata.names.each do |name|
+        metadata_hash[name] = @metadata.get(name) 
+      end
 
-  		metadata_hash
-  	end
+      metadata_hash
+    end
 
-  	def available_metadata
-  		@metadata.names.to_a
-  	end
+    def available_metadata
+      @metadata.names.to_a
+    end
 
-  	def metadata_exists?(name)
-  		if @metadata.get(name) == nil
-  			false
-  		else
-  			true
-  		end
-  	end
+    def metadata_exists?(name)
+      if @metadata.get(name) == nil
+        false
+      else
+        true
+      end
+    end
 
-  	protected
-		
-		def perform
-			input_stream = nil
-			begin
-				input_stream = java.io.FileInputStream.new(java.io.File.new(@filename))
-				@metadata = Metadata.new
-				@metadata.set("filename", File.basename(@filename))
-				@parser = AutoDetectParser.new
-				@content = BodyContentHandler.new
-				@parser.parse(input_stream, @content, @metadata)
-			ensure
-				input_stream.close	
-			end
-		end
+    protected
+    
+    def perform
+      input_stream = nil
+      begin
+        input_stream = java.io.FileInputStream.new(java.io.File.new(@filename))
+        @metadata = Metadata.new
+        @metadata.set("filename", File.basename(@filename))
+        @parser = AutoDetectParser.new
+        @content = BodyContentHandler.new
+        @parser.parse(input_stream, @content, @metadata)
+      ensure
+        input_stream.close  
+      end
+    end
   end
 end
