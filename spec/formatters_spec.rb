5deaafe8b8263884680cf5fa14fa7b89256f67c4
Keith R. Bennett <keithrbennett@gmail.com>
2023-08-24 23:51:47 +0700

    Fix formatters_spec.rb.

--- spec/formatters_spec.rb
+++ spec/formatters_spec.rb
@@ -8,26 +8,16 @@ RF = Rika::Formatters
 describe RF do
   describe '.get' do
     it 'returns the correct formatter for each option character' do
-      expect(RF.get('a')).to eq(RF::AWESOME_PRINT)
-      expect(RF.get('i')).to eq(RF::INSPECT)
-      expect(RF.get('j')).to eq(RF::JSON)
-      expect(RF.get('J')).to eq(RF::PRETTY_JSON)
-      expect(RF.get('t')).to eq(RF::TO_S)
-      expect(RF.get('y')).to eq(RF::YAML)
+      expect(RF.get('a')).to eq(RF::AWESOME_PRINT_FORMATTER)
+      expect(RF.get('i')).to eq(RF::INSPECT_FORMATTER)
+      expect(RF.get('j')).to eq(RF::JSON_FORMATTER)
+      expect(RF.get('J')).to eq(RF::PRETTY_JSON_FORMATTER)
+      expect(RF.get('t')).to eq(RF::TO_S_FORMATTER)
+      expect(RF.get('y')).to eq(RF::YAML_FORMATTER)
     end
 
     it 'raises an error if the option character is invalid' do
       expect { RF.get('x') }.to raise_error(RuntimeError)
-    end
-  end
-
-  describe '.REQUIRED_REQUIRE' do
-    it 'is a hash' do
-      expect(RF::REQUIRED_REQUIRE).to be_a(Hash)
-    end
-
-    it 'has the correct keys' do
-      expect(RF::REQUIRED_REQUIRE.keys).to match_array(%w[a j J y])
     end
   end
 end
