a387be86c21a2980baad210d5cb9393dc5457307
Keith R. Bennett <keithrbennett@gmail.com>
2023-09-01 18:08:39 +0700

    Fix tests for new return value of parse_command_line. Add a test for it too.

--- spec/cli/rika_command_spec.rb
+++ spec/cli/rika_command_spec.rb
@@ -22,56 +22,66 @@ describe RikaCommand do
   end
 
   describe '#parse_command_line' do
-    RSpec.shared_examples 'test_arg_parsing' do |args, option_key, expected_value|
-      specify "correctly sets #{option_key} to #{expected_value} when args are #{args}" do
-        rika_command = RikaCommand.new(args)
-        options = rika_command.send(:parse_command_line)
-        expect(options[option_key]).to eq(expected_value)
-      end
+    specify 'returns a hash of options, a target array,__FILE and help text' do
+      options, targets, help_text = RikaCommand.new([]).send(:parse_command_line)
+      expect(options).to be_a(Hash)
+      expect(targets).to be_an(Array)
+      expect(help_text).to be_a(String)
     end
 
-    # Test default option values:
-    include_examples('test_arg_parsing', [], :as_array, false)
-    include_examples('test_arg_parsing', [], :text, true)
-    include_examples('test_arg_parsing', [], :metadata, true)
-    include_examples('test_arg_parsing', [], :format, 'at')
+    context 'parse_options' do
+      RSpec.shared_examples 'test_arg_parsing' do |args, option_key, expected_value|
+        specify "correctly sets #{option_key} to #{expected_value} when args are #{args}" do
+          rika_command = RikaCommand.new(args)
+          allow(rika_command).to receive(:args).and_return(args)
+          options, _, _ = rika_command.send(:parse_command_line)
+          expect(options[option_key]).to eq(expected_value)
+        end
+      end
 
-    # Test -a as_array option:
-    include_examples('test_arg_parsing', %w[-a], :as_array, true)
-    include_examples('test_arg_parsing', %w[--as_array], :as_array, true)
-    include_examples('test_arg_parsing', %w[-a -a-], :as_array, false)
-    include_examples('test_arg_parsing', %w[-a -a-], :as_array, false)
-    include_examples('test_arg_parsing', %w[--no-as_array], :as_array, false)
+      # Test default option values:
+      include_examples('test_arg_parsing', [], :as_array, false)
+      include_examples('test_arg_parsing', [], :text, true)
+      include_examples('test_arg_parsing', [], :metadata, true)
+      include_examples('test_arg_parsing', [], :format, 'at')
 
-    # Test -f format option:
-    include_examples('test_arg_parsing', %w[-fyy], :format, 'yy')
-    include_examples('test_arg_parsing', %w[--format yy], :format, 'yy')
-    include_examples('test_arg_parsing', %w[-f yy], :format, 'yy')
-    include_examples('test_arg_parsing', %w[-f y], :format, 'yy')
-    include_examples('test_arg_parsing', %w[-f yj], :format, 'yj')
-    include_examples('test_arg_parsing', %w[-f yjJ], :format, 'yj') # Test extra characters after valid format
+      # Test -a as_array option:
+      include_examples('test_arg_parsing', %w[-a], :as_array, true)
+      include_examples('test_arg_parsing', %w[--as_array], :as_array, true)
+      include_examples('test_arg_parsing', %w[-a -a-], :as_array, false)
+      include_examples('test_arg_parsing', %w[-a -a-], :as_array, false)
+      include_examples('test_arg_parsing', %w[--no-as_array], :as_array, false)
 
-    # Test -m metadata option:
-    include_examples('test_arg_parsing', %w[-m- -m], :metadata, true)
-    include_examples('test_arg_parsing', %w[-m- -m+], :metadata, true)
-    include_examples('test_arg_parsing', %w[--metadata false --metadata], :metadata, true)
-    include_examples('test_arg_parsing', %w[-m -m-], :metadata, false)
-    include_examples('test_arg_parsing', %w[-m yes], :metadata, true)
-    include_examples('test_arg_parsing', %w[-m no], :metadata, false)
-    include_examples('test_arg_parsing', %w[-m true], :metadata, true)
-    include_examples('test_arg_parsing', %w[-m false], :metadata, false)
-    include_examples('test_arg_parsing', %w[--metadata false], :metadata, false)
-    include_examples('test_arg_parsing', %w[--no-metadata], :metadata, false)
+      # Test -f format option:
+      include_examples('test_arg_parsing', %w[-fyy], :format, 'yy')
+      include_examples('test_arg_parsing', %w[--format yy], :format, 'yy')
+      include_examples('test_arg_parsing', %w[-f yy], :format, 'yy')
+      include_examples('test_arg_parsing', %w[-f y], :format, 'yy')
+      include_examples('test_arg_parsing', %w[-f yj], :format, 'yj')
+      include_examples('test_arg_parsing', %w[-f yjJ], :format, 'yj') # Test extra characters after valid format
 
-    # Test -t text option:
-    include_examples('test_arg_parsing', %w[-t], :text, true)
-    include_examples('test_arg_parsing', %w[-t -t-], :text, false)
-    include_examples('test_arg_parsing', %w[-t yes], :text, true)
-    include_examples('test_arg_parsing', %w[-t no], :text, false)
-    include_examples('test_arg_parsing', %w[-t true], :text, true)
-    include_examples('test_arg_parsing', %w[-t false], :text, false)
-    include_examples('test_arg_parsing', %w[--text false], :text, false)
-    include_examples('test_arg_parsing', %w[--text false --text], :text, true)
+      # Test -m metadata option:
+      include_examples('test_arg_parsing', %w[-m- -m], :metadata, true)
+      include_examples('test_arg_parsing', %w[-m- -m+], :metadata, true)
+      include_examples('test_arg_parsing', %w[--metadata false --metadata], :metadata, true)
+      include_examples('test_arg_parsing', %w[-m -m-], :metadata, false)
+      include_examples('test_arg_parsing', %w[-m yes], :metadata, true)
+      include_examples('test_arg_parsing', %w[-m no], :metadata, false)
+      include_examples('test_arg_parsing', %w[-m true], :metadata, true)
+      include_examples('test_arg_parsing', %w[-m false], :metadata, false)
+      include_examples('test_arg_parsing', %w[--metadata false], :metadata, false)
+      include_examples('test_arg_parsing', %w[--no-metadata], :metadata, false)
+
+      # Test -t text option:
+      include_examples('test_arg_parsing', %w[-t], :text, true)
+      include_examples('test_arg_parsing', %w[-t -t-], :text, false)
+      include_examples('test_arg_parsing', %w[-t yes], :text, true)
+      include_examples('test_arg_parsing', %w[-t no], :text, false)
+      include_examples('test_arg_parsing', %w[-t true], :text, true)
+      include_examples('test_arg_parsing', %w[-t false], :text, false)
+      include_examples('test_arg_parsing', %w[--text false], :text, false)
+      include_examples('test_arg_parsing', %w[--text false --text], :text, true)
+    end
   end
 
   describe '#versions_string' do
